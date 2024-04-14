7d4afd17d2a2d3ff884f6d739576bf5ee7ba6108
celiaysr <ysrcla@gmail.com>
2024-04-14 15:15:53 +0200

    dependencies added

---
+++ src/main/java/com/celiabilal/projectsport/ProjectSportApplication.java
@@ -0,0 +1,13 @@
+package com.celiabilal.projectsport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/celiabilal/projectsport/ProjectSportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celiabilal.projectsport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
